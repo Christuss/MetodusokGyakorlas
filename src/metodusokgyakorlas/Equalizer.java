cc5b76c27586db5450df953a6ab655385b092c4a
christuss <szendefi.krisztofer@gmail.com>
2023-02-22 13:49:04 +0100

    eq ciklus

--- src/metodusokgyakorlas/Equalizer.java
+++ src/metodusokgyakorlas/Equalizer.java
@@ -12,17 +12,28 @@ import java.util.Random;
  */
 public class Equalizer {
     public static void main(String[] args) {
-        ciklus();
+        feladatok();
+        
     }
 
-    private static void ciklus() {
-        for (int i = 0; i < 5; i++) {
-            eq();
-        }
+    private static void feladatok() {
+        eq();
+        eq(5);
+        eq();
+        eq(11);
+        eq(8);
+    }
+    public static void eq(int szam) {
+        int hossz = szam;
+        kiirasCiklus(hossz);
     }
     public static void eq() {
         Random rnd = new Random();
         int hossz = rnd.nextInt(7-3+1)+3;
+        kiirasCiklus(hossz);
+        
+    }
+    static void kiirasCiklus(int hossz) {
         for (int i = 0; i < hossz; i++) {
             System.out.print("\u001B[45m" + " ");
         }
